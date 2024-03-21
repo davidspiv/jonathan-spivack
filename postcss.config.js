69a78385d7e971ba5a3d8cbc6f052fba60971472
davidspiv <daspivack@gmail.com>
2024-03-21 13:01:02 -0700

    refactored for basic postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
 import postcssImport from 'postcss-import';
-import postcssMixins from 'postcss-mixins';
-import postcssSimpleVars from 'postcss-simple-vars';
-import postcssNested from 'postcss-nested';
 import postcssPresetEnv from 'postcss-preset-env';
 import cssNano from 'cssnano';
 
 export default {
-  plugins: [
-    postcssImport,
-    postcssMixins,
-    postcssSimpleVars,
-    postcssNested,
-    postcssPresetEnv,
-    cssNano,
-  ],
+  plugins: [postcssImport, postcssPresetEnv, cssNano],
 };
