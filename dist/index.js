4389d5bfc24cc1752a03fa0f185aa7e0cd66390f
davidspiv <daspivack@gmail.com>
2024-03-22 13:29:02 -0700

    added video

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+const mediaContainer = document.querySelector('#media-container');
+const mediaButtonContainer = document.querySelector('#media-article ul');
+const linkArr = [
+  'a-whiter-shade-of-pale',
+  'besame-mucho',
+  'blackbird-and-strawberry-fields',
+  'comin-home-baby',
+  'exactly-like-you',
+  'get-together',
+  'the-water-is-wild',
+  'wayfaring-stranger',
+  'what-a-wonderful-world',
+];
+mediaButtonContainer.addEventListener('click', function (e) {
+  const pressedButtonId = e.target.id;
+  if (pressedButtonId) {
+    const linkTitle = linkArr[Number(pressedButtonId.slice(-1)) - 1];
+    const src = `https://davidsmediabucket.s3.us-west-1.amazonaws.com/jonathan-spivack/video/${linkTitle}.mp4`;
+    mediaContainer.innerHTML = `
+    <video width="640px" height="360px" controls="controls">
+      <source
+        src=${src}
+        type="video/mp4"
+      />
+    </video>
+    <h3>Test</h3>
+        `;
+  }
+});
