6903b4263d595800b18de1519750ba00e43a9b9e
davidspiv <daspivack@gmail.com>
2024-05-17 12:28:28 -0700

    refactored favoring biome over prettier, postcss, eslint dumpster fire

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,22 @@
-const mediaContainer = document.querySelector('#media-container');
-const mediaButtonContainer = document.querySelector('#media-article ul');
+const mediaContainer = document.querySelector("#media-container");
+const mediaButtonContainer = document.querySelector("#media-article ul");
 const linkArr = [
-  'a-whiter-shade-of-pale',
-  'besame-mucho',
-  'blackbird-and-strawberry-fields',
-  'comin-home-baby',
-  'exactly-like-you',
-  'get-together',
-  'the-water-is-wild',
-  'wayfaring-stranger',
-  'what-a-wonderful-world',
+	"a-whiter-shade-of-pale",
+	"besame-mucho",
+	"blackbird-and-strawberry-fields",
+	"comin-home-baby",
+	"exactly-like-you",
+	"get-together",
+	"the-water-is-wild",
+	"wayfaring-stranger",
+	"what-a-wonderful-world",
 ];
-mediaButtonContainer.addEventListener('click', function (e) {
-  const pressedButtonId = e.target.id;
-  if (pressedButtonId) {
-    const linkTitle = linkArr[Number(pressedButtonId.slice(-1)) - 1];
-    const src = `https://davidsmediabucket.s3.us-west-1.amazonaws.com/jonathan-spivack/video/${linkTitle}.mp4`;
-    mediaContainer.innerHTML = `
+mediaButtonContainer.addEventListener("click", (e) => {
+	const pressedButtonId = e.target.id;
+	if (pressedButtonId) {
+		const linkTitle = linkArr[Number(pressedButtonId.slice(-1)) - 1];
+		const src = `https://davidsmediabucket.s3.us-west-1.amazonaws.com/jonathan-spivack/video/${linkTitle}.mp4`;
+		mediaContainer.innerHTML = `
     <video width="640px" height="360px" controls="controls">
       <source
         src=${src}
@@ -25,5 +25,5 @@ mediaButtonContainer.addEventListener('click', function (e) {
     </video>
     <h3>${e.target.textContent}</h3>
         `;
-  }
+	}
 });
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-import postcssImport from 'postcss-import';
-import autoprefixer from 'autoprefixer';
-import cssNano from 'cssnano';
-
-export default {
-  plugins: [postcssImport, autoprefixer, cssNano],
-};
