eb46b2de6c7a163995ed6435cdb8d34e8cdc1af1
davidspiv <daspivack@gmail.com>
2024-05-20 20:30:17 -0700

    white color

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,17 @@
-const mediaContainer = document.querySelector("#media-container");
-const mediaButtonContainer = document.querySelector("#media-article ul");
+const mediaContainer = document.querySelector('#media-container');
+const mediaButtonContainer = document.querySelector('#media-article ul');
 const linkArr = [
-	"a-whiter-shade-of-pale",
-	"besame-mucho",
-	"blackbird-and-strawberry-fields",
-	"comin-home-baby",
-	"exactly-like-you",
-	"get-together",
-	"the-water-is-wild",
-	"wayfaring-stranger",
-	"what-a-wonderful-world",
+	'a-whiter-shade-of-pale',
+	'besame-mucho',
+	'blackbird-and-strawberry-fields',
+	'comin-home-baby',
+	'exactly-like-you',
+	'get-together',
+	'the-water-is-wild',
+	'wayfaring-stranger',
+	'what-a-wonderful-world',
 ];
-mediaButtonContainer.addEventListener("click", (e) => {
+mediaButtonContainer.addEventListener('click', (e) => {
 	const pressedButtonId = e.target.id;
 	if (pressedButtonId) {
 		const linkTitle = linkArr[Number(pressedButtonId.slice(-1)) - 1];
